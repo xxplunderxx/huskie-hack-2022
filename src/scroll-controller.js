a6c68f8f4abe1e82dc9161b2cb241748245e3824
David Petrovski <petrovskidavid1@gmail.com>
2022-11-06 02:25:56 -0600

    Added text, and css animations for scrollytelling

--- src/scroll-controller.js
+++ src/scroll-controller.js
@@ -1,13 +1,10 @@
 // Gets all DOMs
 var main = document.querySelector("main");
-var redlining = main.querySelector("#redlining");
+var redlining = main.querySelector("#scrolly");
 var redliningSticky = redlining.querySelector(".redlining-sticky-img");
 var redliningArticle = redlining.querySelector("article");
-var redliningSteps = redliningArticle.querySelectorAll(".redlining-step");
+var redliningSteps = redliningArticle.querySelectorAll(".step");
 
-var foodDesert = main.querySelector("#food-desert");
-var foodDesertArticle = foodDesert.querySelector("article");
-var foodDesertSteps = foodDesertArticle.querySelectorAll(".food-desert-step");
 
 // Initializes the scrollama
 var scroller1 = scrollama();
@@ -17,41 +14,21 @@ function handleStepEnterRedlining(response) {
   var currentStep = response.element;
 
   // Removes previous step, and sets current step to active
-  foodDesertSteps.forEach((step) => step.classList.remove("is-active"));
   redliningSteps.forEach((step) => step.classList.remove("is-active"));
   currentStep.classList.add("is-active");
 }
 
-// Initializes the scrollama
-var scroller2 = scrollama();
 
-// Handles the entering of the next step
-function handleStepEnterFoodDesert(response) {
-  var currentStep = response.element;
-
-  // Removes previous step, and sets current step to active
-  redliningSteps.forEach((step) => step.classList.remove("is-active"));
-  foodDesertSteps.forEach((step) => step.classList.remove("is-active"));
-  currentStep.classList.add("is-active");
-}
 
 // Initialized the scroller and event listeners
 function init() {
   scroller1
     .setup({
-      step: "#redlining article .redlining-step",
-      offset: 0.33,
+      step: "#scrolly article .step",
+      offset: 0.55,
       debug: true,
     })
     .onStepEnter(handleStepEnterRedlining);
-
-  scroller2
-    .setup({
-      step: "#food-desert article .food-desert-step",
-      offset: 0.33,
-      debug: true,
-    })
-    .onStepEnter(handleStepEnterFoodDesert);
 
   // Checks if window is resized and resizes
   window.addEventListener("resize", scroller1.resize);
