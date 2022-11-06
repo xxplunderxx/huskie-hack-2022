5886c9a3a8137f14cef7b510a2efdce09c96cb5c
xxplunderxx <71054045+xxplunderxx@users.noreply.github.com>
2022-11-06 01:02:07 -0500

    working commit

--- src/scroll-controller.js
+++ src/scroll-controller.js
@@ -1,40 +1,60 @@
 // Gets all DOMs
 var main = document.querySelector("main");
-var scrolly = main.querySelector("#scrolly");
-var sticky = scrolly.querySelector(".sticky-thing");
-var article = scrolly.querySelector("article");
-var steps = article.querySelectorAll(".step");
+var redlining = main.querySelector("#redlining");
+var redliningSticky = redlining.querySelector(".redlining-sticky-img");
+var redliningArticle = redlining.querySelector("article");
+var redliningSteps = redliningArticle.querySelectorAll(".redlining-step");
 
+var foodDesert = main.querySelector("#food-desert");
+var foodDesertArticle = foodDesert.querySelector("article");
+var foodDesertSteps = foodDesertArticle.querySelectorAll(".food-desert-step");
 
 // Initializes the scrollama
-var scroller = scrollama();
-
+var scroller1 = scrollama();
 
 // Handles the entering of the next step
-function handleStepEnter(response) {
-    var currentStep = response.element;
+function handleStepEnterRedlining(response) {
+  var currentStep = response.element;
 
-    // Removes previous step, and sets current step to active
-    steps.forEach(step => step.classList.remove('is-active'));
-    currentStep.classList.add('is-active');
-
-    // Updates background square <p> to the data from current step
-    sticky.querySelector("p").innerText = currentStep.dataset.step;
+  // Removes previous step, and sets current step to active
+  foodDesertSteps.forEach((step) => step.classList.remove("is-active"));
+  redliningSteps.forEach((step) => step.classList.remove("is-active"));
+  currentStep.classList.add("is-active");
 }
 
+// Initializes the scrollama
+var scroller2 = scrollama();
+
+// Handles the entering of the next step
+function handleStepEnterFoodDesert(response) {
+  var currentStep = response.element;
+
+  // Removes previous step, and sets current step to active
+  redliningSteps.forEach((step) => step.classList.remove("is-active"));
+  foodDesertSteps.forEach((step) => step.classList.remove("is-active"));
+  currentStep.classList.add("is-active");
+}
 
 // Initialized the scroller and event listeners
 function init() {
-    scroller
-        .setup({
-            step: "#scrolly article .step",
-            offset: 0.33,
-            debug: true
-        })
-        .onStepEnter(handleStepEnter);
+  scroller1
+    .setup({
+      step: "#redlining article .redlining-step",
+      offset: 0.33,
+      debug: true,
+    })
+    .onStepEnter(handleStepEnterRedlining);
 
-    // Checks if window is resized and resizes
-    window.addEventListener("resize", scroller.resize);
+  scroller2
+    .setup({
+      step: "#food-desert article .food-desert-step",
+      offset: 0.33,
+      debug: true,
+    })
+    .onStepEnter(handleStepEnterFoodDesert);
+
+  // Checks if window is resized and resizes
+  window.addEventListener("resize", scroller1.resize);
 }
 
 init();
